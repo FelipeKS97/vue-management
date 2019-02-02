9e4d6e160b39c57b2eabf23eb39e14ef96d52da0
Felipe Carvalho <felipecavalcanti100@gmail.com>
2019-02-02 17:26:29 -0300

    Adicinando css e View de login

---
+++ src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+<div id="login-page">
+  <div id="login">
+    <div id="description">
+      <h1>Login</h1>
+      <p>Bem Vindo! Realize seu login no sistema com E-mail e Senha.</p>
+    </div>
+    <div id="form">
+      <form @submit.prevent="doLogin">
+        <label for="email">E-mail</label>
+        <input type="text" id="email" v-model="email" placeholder="exemplo@exemplo.com" autocomplete="off">
+
+        <label for="password">Senha</label>&nbsp;
+        <i class="fas" :class="[passwordIcon]" @click="hidePassword = !hidePassword"></i>
+        <input :type="passwordType" id="password" v-model="password" placeholder="**********">
+
+        <button type="submit">Acessar</button>
+      </form>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'login',
+    data: function() {
+        return {
+            email: '',
+            password: '',
+            hidePassword: true,
+            isSignUp: false
+
+        }
+    },
+    computed: {
+        passwordHide() {
+            return this.hidePassword ? 'password' : 'text'
+        },
+        passwordIcon() {
+            return this.hidePassword ? 'eye' : 'eye-off'
+        }
+    },
+    methods: {
+        doLogin() {
+        alert('Not yet')
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import '../css/login.css';
+</style>
